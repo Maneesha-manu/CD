7f4ee5269cfe434b5aba35448c8812245ab7030d
Maneesha-manu <101265102+Maneesha-manu@users.noreply.github.com>
2023-03-24 11:33:56 +0530

    Add files via upload

---
+++ exp7.c
@@ -0,0 +1,124 @@
+#include<stdio.h>
+#include<string.h>
+
+int k=0,z=0,i=0,j=0,c=0;
+char a[16],ac[20],stk[15],act[10];
+void check();
+
+void main() {
+    printf("Grammar is : \nE->E+E\nE->E*E\nE->(E)\nE->id\n");
+    printf("Enter an input string : ");
+    scanf("%s",a);
+    c = strlen(a);
+    strcpy(act,"SHIFT -> ");
+    puts("Stack \t Input \t\t Action ");
+    puts("----- \t ----- \t\t ------ ");
+    for(k=0,i=0; j<c ;k++,i++,j++) {
+        if(a[j] == 'i' && a[j+1] == 'd') {
+            stk[i] = a[j];
+            stk[i+1] = a[j+1];
+            stk[i+2] = '\0';
+            a[j] = ' ';
+            a[j+1] = ' ';
+            printf("\n$%s\t%s$\t%sid",stk,a,act);
+            check();
+        } else {
+            stk[i] = a[j];
+            stk[i+1] = '\0';
+            a[j] = ' ';
+            printf("\n$%s\t%s$\t%s symbols",stk,a,act);
+            check();
+        }
+    }
+    printf("\n");
+}
+
+void check() {
+    strcpy(ac, "REDUCE TO E");
+    for(z=0;z<c;z++) {
+        if(stk[z] == 'i' && stk[z+1] == 'd') {
+            stk[z] = 'E';
+            stk[z+1] = '\0';
+            printf("\n$%s\t%s$\t%s",stk,a,ac);
+            j++;
+        }
+    }
+
+    for(z=0;z<c;z++) {
+        if(stk[z] == 'E' && stk[z+1] == '+' && stk[z+2] == 'E') {
+            stk[z] = 'E';
+            stk[z+1] = '\0';
+            stk[z+2] = '\0';
+            printf("\n$%s\t%s$\t%s",stk,a,ac);
+            i=i-2;
+        }
+    }
+
+    for(z=0;z<c;z++) {
+        if(stk[z] == 'E' && stk[z+1] == '*' && stk[z+2] == 'E') {
+            stk[z] = 'E';
+            stk[z+1] = '\0';
+            stk[z+2] = '\0';
+            printf("\n$%s\t%s$\t%s",stk,a,ac);
+            i=i-2;
+        }
+    }
+
+    for(z=0;z<c;z++) {
+        if(stk[z] == '(' && stk[z+1] == 'E' && stk[z+2] == ')') {
+            stk[z] = 'E';
+            stk[z+1] = '\0';
+            stk[z+2] = '\0';
+            printf("\n$%s\t%s$\t%s",stk,a,ac);
+            i=i-2;
+        }
+    }
+}
+
+
+
+
+---------------------------------------OUTPUT----------------------------
+Grammar is : 
+E->E+E
+E->E*E
+E->(E)
+E->id
+Enter an input string : id+id*id
+Stack 	 Input 		 Action 
+----- 	 ----- 		 ------ 
+
+$id	  +id*id$	SHIFT -> id
+$E	  +id*id$	REDUCE TO E
+$E+	   id*id$	SHIFT ->  symbols
+$E+id	     *id$	SHIFT -> id
+$E+E	     *id$	REDUCE TO E
+$E	     *id$	REDUCE TO E
+$E*	      id$	SHIFT ->  symbols
+$E*id	        $	SHIFT -> id
+$E*E	        $	REDUCE TO E
+$E	        $	REDUCE TO E
+
+
+Grammar is : 
+E->E+E
+E->E*E
+E->(E)
+E->id
+Enter an input string : id*id*id
+Stack 	 Input 		 Action 
+----- 	 ----- 		 ------ 
+
+$id	  *id*id$	SHIFT -> id
+$E	  *id*id$	REDUCE TO E
+$E*	   id*id$	SHIFT ->  symbols
+$E*id	     *id$	SHIFT -> id
+$E*E	     *id$	REDUCE TO E
+$E	     *id$	REDUCE TO E
+$E*	      id$	SHIFT ->  symbols
+$E*id	        $	SHIFT -> id
+$E*E	        $	REDUCE TO E
+$E	        $	REDUCE TO E
+
+
+
